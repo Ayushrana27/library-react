7ab573d821d029486df481e4f8ba7e35b8f810bd
Ayush Rana <162277875+Ayushrana27@users.noreply.github.com>
2025-09-19 21:45:02 +0530

    Update SearchBar.jsx

--- SearchBar.jsx
+++ SearchBar.jsx
@@ -1,1 +1,13 @@
-
+function SearchBar({ searchTerm, setSearchTerm }) {
+    return (
+      <input
+        type="text"
+        placeholder="Search by title or author"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="search-input"
+      />
+    );
+  }
+  
+  export default SearchBar;
