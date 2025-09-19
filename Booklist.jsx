52406b18da3dc85d687dd11d117522230870160e
Ayush Rana <162277875+Ayushrana27@users.noreply.github.com>
2025-09-19 21:42:41 +0530

    Update Booklist.jsx

--- Booklist.jsx
+++ Booklist.jsx
@@ -1,1 +1,14 @@
-
+function BookList({ books, onRemoveBook }) {
+    return (
+      <div className="book-list">
+        {books.map((book) => (
+          <div key={book.id} className="book-item">
+            <strong>{book.title}</strong> by {book.author}
+            <button onClick={() => onRemoveBook(book.id)}>Remove</button>
+          </div>
+        ))}
+      </div>
+    );
+  }
+  
+  export default BookList;
